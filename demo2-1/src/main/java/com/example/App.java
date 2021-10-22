fb0679ff75814a20d76ba7fddf9aeb00a4a73842
Risona <Risona@runa.com>
2021-10-22 17:31:17 +0800

    risona 2 commit

--- demo2-1/src/main/java/com/example/App.java
+++ demo2-1/src/main/java/com/example/App.java
@@ -9,6 +9,7 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
 
     }
 
