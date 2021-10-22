485dd8d8fb5a4f5a3ca9f1197f98a06170ee6976
Risona <Risona@runa.com>
2021-10-22 18:25:52 +0800

    risona master commit 3

--- demo2-1/src/main/java/com/example/App.java
+++ demo2-1/src/main/java/com/example/App.java
@@ -14,6 +14,7 @@ public class App {
         System.out.println("Hello World!");
         System.out.println("dev Hello World!");
         System.out.println("master Hello World!");
+        System.out.println("master Hello World!");
 
     }
 
